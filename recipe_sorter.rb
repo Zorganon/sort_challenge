df118cfff3a6ad705d44986dbae51e6b3319ab36
Zorganon <dan.the.hudson@gmail.com>
2017-01-23 17:44:45 -0600

    added Sinatra

--- recipe_sorter.rb
+++ recipe_sorter.rb
@@ -1,28 +1,22 @@
+#assumptions
+#categories are one word
+#no apostrophes in recipes
+#servings are not spelled out
 
+#RecipeArray should store recipes as named Hashes
 
-class RecipeSorterApp
-@@recipe_counter = 0
+require 'sinatra'
+require 'json'
+
+module sortAble
+
+end
+
+class RecipeBox
+
 	def fileGetter
 
-		this method should take the recipe files and put them into an array which can be accessed by other methods?
-	end
-
-	def recipesIn
-		File.foreach('recipes_pipe') { |line| 
-			string_line = line.split('|')
-			recipe[@@recipe_counter] = saveRecipe(string_line)
-			@@recipe_counter += 1
-
-		}
-		File.foreach('recipes_comma') { |line|
-			string_line = line.split(',')
-			saveRecipe(string_line)
-		}
-		File.foreach('recipes_space') { |line|
-			new_record = Hash.new 
-			new_record[name] = line.match(/^'[\w\s]+'/)
-			new_record[servings] = line.match(/'[\w\s]+'\w+$/)
-		}
+		#this method should take the recipe files and put them into an array which can be accessed by other methods?
 	end
 
 	def saveRecipe(line)
@@ -34,3 +28,43 @@ class RecipeSorterApp
 		return new_record
 	end
 
+end
+
+class Recipe	
+	def initialize(name, category, cooktime, servings)
+		@name = name
+		@category = category
+		@cooktime = cooktime
+		@servings = servings
+	end
+
+	def <=>(attribute)
+		if attribute == "cooktime"
+			compare = self.select(/[\d]{1,4}/)
+		end
+	end
+end
+
+##### API stuff #####
+set :port, 8080
+set :environment, :production
+
+get '/read_in' do
+	fileGetter
+end
+
+get '/sort' do
+	#view that offers options of 
+end
+
+post '/sort/name' do
+end
+
+post '/sort/category' do
+end
+
+post '/sort/cooktime' do
+end
+
+post '/sort/servings' do
+end
