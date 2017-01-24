d3fa9f794fc70f89fdc18488b8c0b13352585fca
Zorganon <dan.the.hudson@gmail.com>
2017-01-23 19:05:51 -0600

    added sort logic feeding on params

--- recipe_sorter.rb
+++ recipe_sorter.rb
@@ -1,4 +1,4 @@
-#assumptions
+###assumptions!
 #categories are one word
 #no apostrophes in recipes
 #servings are not spelled out
@@ -7,36 +7,41 @@
 
 require 'sinatra'
 
+#Recipe Box Class
 class RecipeBox < Array
-
-	def sortRecipes(attribute, recipeArray)
-		if self.empty?
-			puts "Box is empty dumbass."
-		else
-			if attribute == "name"
-				response = RestClient.get 'http://localhost:8080/sort_name'
-			elsif attribute == "category"
-				response = RestClient.get 'http://localhost:8080/sort_category'
-			elsif attribute == "cook_time"
-				response = RestClient.get 'http://localhost:8080/sort_cooktime'
-			elsif attribute == "servings"
-				response = RestClient.get 'http://localhost:8080/sort_servings'
-			end	
-		end
-	end
-
-	def <=>(attribute)
-	end
 
 	def getRecipes
 		response = RestClient.get 'http://localhost:8080/get_recipes'
 	end
 
+	def sortRecipes(attribute)
+		if self.empty?
+			puts "Box is empty dumbass."
+		else
+			response = RestClient.post 'http://localhost:8080/sort', {:params => {:attribute => attribute}} 
+			puts response				
+		end
+	end
 
-
+	def rsort(attribute)
+		if attribute == :name
+			self.sort_by{|x,y| x.name <=> y.name}
+		elsif attribute == :category
+			self.sort_by{|x,y| x.category <=> y.category}
+		elsif attribute == :cooktime
+			self.sort_by{|x,y| x.cooktime.select(/[\d]{1,4}/) <=> y.cooktime.select(/[\d]{1,4}/)}
+		elsif attribute == :servings
+			self.sort_by{|x,y| x.servings <=> y.servings}
+		else
+			return "bad search criteria"
+		end
 end
+	
 
+#Recipe Class	
 class Recipe	
+	attr_accessor :name, :category, :cooktime, :servings
+
 	def initialize(name, category, cooktime, servings)
 		@name = name
 		@category = category
@@ -44,28 +49,20 @@ class Recipe
 		@servings = servings
 	end
 
+	def show
+		puts "#{@name} - #{@category} - takes #{@cooktime} - Serves #{@servings}"
 end
 
 ##### API stuff #####
 set :port, 8080
 set :environment, :production
 
-get '/get_recipes' do
-	
+get '/get_recipes' do	
 end
 
-post '/sort/name' do
-	i = 0
-	while i < (recipeArray.length - 1) do
-		recipeArray[i].<=>(attribute, recipeArray[i+1])
+post '/sort' do
+	attribute = JSON.parse(params[:attribute], :symbolize_names => true)
+	recipe_box.rsort(attribute)
+	recipe_box.each do each.show
 	end
-end
-
-post '/sort/category' do
-end
-
-post '/sort/cooktime' do
-end
-
-post '/sort/servings' do
 end
