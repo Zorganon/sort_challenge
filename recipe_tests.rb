5214adc45ca0a41bd549cef246b587d32fb9965f
Zorganon <dan.the.hudson@gmail.com>
2017-02-07 12:30:16 -0600

    added tests

--- recipe_tests.rb
+++ recipe_tests.rb
@@ -1,6 +1,5 @@
 ENV['RACK_ENV'] = 'test'
 
-require 'recipe_sorter.rb'
 require 'recipe_client.rb'
 require 'test/unit'
 require 'rack/test'
@@ -12,7 +11,37 @@ class RecipeTests < Test::Unit::TestCase
 		sinatra::application
 	end
 
-	def test_adding_a_recipe
-		post '/recipe'
+	def test_box_creation
+		box = BoxClient.new('box')
+		box.getRecipes
+		
+		assert_block( failure_message = "reading comma recipes failed") do
+			box.each do 
+				if each.include?("borscht")
+					return true
+				end
+			end
+		end
+		assert_block(failure_message = "reading pipe recipes failed") do
+			box.each do
+				if each.include?("cream cheese wontons")
+					return true
+				end
+			end
+		end
+		assert_block(failure_message = "reading space recipes failed") do
+			box.each do
+				if each.include?("whale biryani")
+					return true
+				end
+			end
+		end
 	end
+
+	def test_sort_by_name
+		box = BoxClient.new('box')
+		box.getRecipes
+		box.sortRecipes(name)
+	end
+
 end
